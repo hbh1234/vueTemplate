d98f63e273c312d5f06feb656fd860260869fed5
hbh <hebh@glodon.com>
2022-02-02 15:17:32 +0800

    createtes

--- src/views/Home.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    33333
-  </div>
-</template>
--- vue.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  lintOnSave: false, // 关闭代码检查
-};
